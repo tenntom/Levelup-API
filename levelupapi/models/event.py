3c568e5f53d489fe3b94ba81a91ef4daea75f19f
Tom Clouse <tommyclouse@yahoo.com>
2021-08-05 08:48:02 -0500

    First commit

---
+++ levelupapi/models/event.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Event(models.Model):
+    host = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+    game = models.ForeignKey("Game", on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    description = models.TextField()
+    title = models.CharField(max_length=100)
+    attendees = models.ManyToManyField("Gamer", through="EventGamer", related_name="attending")
---
+++ levelupapi/models/game.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils.translation import get_language_from_request
+
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    game_type = models.ForeignKey("GameType", on_delete=models.CASCADE)
+    description = models.CharField(max_length = 150)
+    number_of_players = models.IntegerField()
+    gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+    maker = models.CharField(max_length=50)

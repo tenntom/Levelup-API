b312f7dd43b58b8cb15539c63ce5248aec099e8d
Tom Clouse <tommyclouse@yahoo.com>
2021-08-24 15:01:20 -0500

    first commit

--- levelup/urls.py
+++ levelup/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('levelupreports.urls')),
 ]
---
+++ levelupreports/views/__init__.py
@@ -0,0 +1,4 @@
+from levelupreports.views.connection import Connection
+from .connection import Connection
+from .users.gamesbyuser import usergame_list
+

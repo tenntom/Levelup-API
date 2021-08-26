a853b47a307e3e50f95e3e5ef4670c9fd8a5bb14
Tom Clouse <tommyclouse@yahoo.com>
2021-08-25 19:50:44 -0500

    end of day

--- Levelup.sql
+++ Levelup.sql
@@ -1,1 +1,19 @@
 SELECT * FROM levelupapi_game;
+
+SELECT
+    e.id,
+    e.title,
+    e.description,
+    e.date,
+    e.time,
+    e.attendee
+    gr.id user_id,
+    u.first_name || ' ' || u.last_name AS full_name
+FROM
+    levelupapi_event e
+JOIN
+    levelupapi_gamer gr ON e.host_id = gr.id
+JOIN
+    auth_user u ON gr.user_id = u.id
+
+                    
